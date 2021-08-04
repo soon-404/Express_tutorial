bda69bcd56843496ce4ad98b0c529a0b36c7fd68
soon-404 <tfpsoon@gmail.com>
2021-08-04 12:10:49 +0700

    addUser

--- index.js
+++ index.js
@@ -8,20 +8,12 @@ const app = express();
 // Init Middleware
 app.use(logger);
 
-// Get all users
-app.get("/api/users", (req, res) => {
-  res.json(users);
-});
+// Body parse middleware ตอน post ให้ข้อมูลแสดงออกมา
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Get single users
-app.get("/api/users/:id", (req, res) => {
-  let found = users.some((user) => user.id == req.params.id);
-  if (found) {
-    res.json(users.filter((user) => user.id == req.params.id));
-  } else {
-    res.status(400).json({ msg: `No users with the id of ${req.params.id}` });
-  }
-});
+// Route
+app.use("/api/users", require("./routes/api/users"));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
